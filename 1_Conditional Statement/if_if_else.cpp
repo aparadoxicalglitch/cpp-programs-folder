866244840cbbce8b5c78879bf3192978ade7f5a6
Himanshu <himanshukr.13254@gmail.com>
2022-05-04 13:40:53 +0530

    updating conditional statement folder

---
+++ 1_Conditional Statement/if_if_else.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // int a = 9;
+    // if(a == 9)
+    // {
+    //     cout<<"Nine"<<endl;
+    // }
+    // if(a>0)
+    // {
+    //     cout<<"Positive"<<endl;
+    // }
+    // else
+    // {
+    //     cout<<"Negative"<<endl;
+    // }
+
+    // int a = 2;
+    // int b = a+1;
+
+    // if((a=3)==b)
+    // {
+    //     cout<<a;  // this part will be executed
+    // }
+    // else
+    // {
+    //     cout<<a+2;
+    // }
+
+    int a = 23;
+
+    if(a>20)
+    {
+        cout<<"Hey";
+    }
+    else if(a==23)
+    {
+        cout<<"Hi";
+    }
+    else
+    {
+        cout<<"There";
+    }
+
+    return 0;
+}
