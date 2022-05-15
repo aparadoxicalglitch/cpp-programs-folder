1e0830a64236abeba6590f48b4616a9dc536cc90
Himanshu <himanshukr.13254@gmail.com>
2022-05-15 12:14:02 +0530

    Queue

---
+++ 11_STL/queue.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+int main()
+{
+
+    queue<string> q;
+
+    q.push("Himanshu");
+    q.push("Kumar");
+    q.push("Hii");
+
+    cout << "Size before pop" << q.size() << endl;
+
+    cout << "First Element " << q.front() << endl;
+    q.pop();
+    cout << "First Element " << q.front() << endl;
+
+    cout << "Size after pop" << q.size() << endl;
+}
