a0e93300f96085716b3da986d6e3431a12cdaec5
Himanshu <himanshukr.13254@gmail.com>
2022-06-06 19:11:07 +0530

    updating recursion folder

--- 13_Recursion/walkingExample.cpp
+++ 13_Recursion/walkingExample.cpp
@@ -1,24 +1,26 @@
-#include<iostream> 
+#include <iostream>
 using namespace std;
 
-void reachHome(int src, int dest) {
-    
-    cout << "source " << src  << " destination " << dest << endl;
-    //base case
-    if(src == dest) {
+void reachHome(int src, int dest)
+{
+
+    cout << "source " << src << " destination " << dest << endl;
+    // base case
+    if (src == dest)
+    {
         cout << " pahuch gya " << endl;
-        return ;
+        return;
     }
 
-    //processing - ek step aage badhjao
+    // processing - ek step aage badhjao
     src++;
 
-    //recursive call
+    // recursive call
     reachHome(src, dest);
-
 }
 
-int main() {
+int main()
+{
 
     int dest = 10;
     int src = 1;
@@ -26,7 +28,6 @@ int main() {
     cout << endl;
 
     reachHome(src, dest);
-
 
     return 0;
 }
